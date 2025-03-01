61b6023df350c8d2f989c540f77cada516811c22
Tommer Schwarz <tommerschwarz@Tommers-MBP.cable.rcn.com>
2025-02-28 21:46:37 -0500

    new app

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ import os
 
 class Config:
     SECRET_KEY = os.urandom(24)
-    SPOTIFY_CLIENT_ID = '28fbc39f2018449ab318877c240f597f'
-    SPOTIFY_CLIENT_SECRET = 'f59b9f2edd3047aa817e3e0e0217ecf5'
-    SPOTIFY_REDIRECT_URI = 'https://altavia.onrender.com'
+    SPOTIFY_CLIENT_ID = '2ff161149174488e8582dc0dd941fcb4'
+    SPOTIFY_CLIENT_SECRET = 'b5fa33c9a4674829acb272d34594d5fa'
+    SPOTIFY_REDIRECT_URI = 'https://altavia.onrender.com/callback'
 
 print(f"Using Spotify Client ID: {Config.SPOTIFY_CLIENT_ID}")
 print(f"Redirect URI: {Config.SPOTIFY_REDIRECT_URI}")
