ca5ca2f14c9b76527cf01d6e813ed7e75aea07d5
Tommer Schwarz <tommerschwarz@Tommers-MBP.lan>
2025-03-09 22:50:54 -0400

    default playlist selection 10

--- app/static/js/dashboard.js
+++ app/static/js/dashboard.js
@@ -386,7 +386,7 @@ window.PlaylistDashboard = () => {
                 color: playlistData.map((p, index) => {
                     // If it's the user's own playlist (use index to match with label)
                     // if (userPlaylist && (p === userPlaylist || playlistLabels[index].includes(`${currentUsername} in ${selectedYear}`))) {
-                    if (userPlaylist) {
+                    if (p === userPlaylist) {
                         return '#cb6d51';  // Terra cotta for user's playlist
                     }
                     
